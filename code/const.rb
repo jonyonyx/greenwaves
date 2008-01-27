5063b594718e03c76530113579f75434e5a8bb72
andreas.warberg <andreas.warberg@bbe71f41-d438-0410-94a7-597091fd6986>
2008-01-27 19:12:43 +0000

    Begun connector and link parsing for route enumeration

--- code/const.rb
+++ code/const.rb
@@ -35,13 +35,56 @@ class Csvtable < Array
     reader.close
   end
 end
+    
+class VissimElem
+  attr_reader :type,:number,:name
+  
+  # total proportion request by all elems of each type
+  @@total_prop = Hash.new(0.0) 
+  def initialize type,number,name,prop
+    @type,@number,@name,@prop = type,number,name,prop
+    @@total_prop[@type] = @@total_prop[@type] + prop
+  end
+  def proportion
+    @prop / @@total_prop[@type]
+  end
+  def to_s
+    "#{@type} #{@number} #{format('%f', proportion)} '#{@name}'"
+  end
+end
+
+class Link < VissimElem
+  attr_reader :direction,:has_buses
+  def initialize number,name,direction,rel_proportion = 0.0, bus_input = 'N'
+    super 'LINK',number,name,rel_proportion
+    @direction = direction
+    @has_buses = bus_input == 'Y' # are buses inserted on this link?
+  end
+  def to_s
+    "#{@type} #{@number} #{format('%f', proportion)} '#{@name} #{@direction}'"
+  end
+end
+class Composition < VissimElem
+  def initialize number,name,rel_proportion
+    super 'COMPOSITION',number,name,rel_proportion
+  end
+end
+
+class VissimFun
+  def VissimFun.get_links area_name
+    links = []
+    Csvtable.enumerate("#{Vissim_dir}#{area_name}_input_links.csv") do |row|
+      links << Link.new(
+        row['number'].to_i, 
+        row['name'], 
+        row['direction'], 
+        row['rel_flow'].to_f, 
+        row['bus_input'])
+    end
+    links
+  end
+end
 
 if $0 == __FILE__ 
-  #tbl = Csvtable.new "#{Vissim_dir}compositions.csv"
-  #puts tbl.header.inspect
-  #puts tbl.inspect
-  
-  Csvtable.enumerate("#{Vissim_dir}herlev_input_links.csv") do |row|
-    puts row['number']
-  end
+  puts VissimFun.get_links('herlev')
 end
