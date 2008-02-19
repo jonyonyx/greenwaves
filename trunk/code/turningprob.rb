e0b77d8834f9d8a93507deac5743d8341688921b
andreas.warberg <andreas.warberg@bbe71f41-d438-0410-94a7-597091fd6986>
2008-02-19 00:39:05 +0000

    clean coded most of traffic counts. long way in rb code to extract turning percentage info from it!

--- trunk/code/turningprob.rb
+++ trunk/code/turningprob.rb
@@ -1,11 +1,23 @@
 require 'const'
 require 'dbi'
 
-Count_xls = "../data/tællinger/counts.xls"
+Count_xls = "../data/counts/counts.xls"
 CS = "DBI:ADO:Provider=Microsoft.Jet.OLEDB.4.0;Data Source=#{Count_xls};Extended Properties=\"Excel 8.0;HDR=Yes;IMEX=1\";"
 
+SQL = "SELECT Intersection, [From], 
+        SUM([Cars Left]) / SUM([Total Cars]) AS L,
+        SUM([Cars Through]) / SUM([Total Cars]) AS T,
+        SUM([Cars Right]) / SUM([Total Cars]) AS R
+       FROM [data$] 
+       GROUP BY Intersection,[From]"
+
 DBI.connect(CS) do |dbh|  
-  Count_rows = dbh.select_all "SELECT * FROM [data$]"  
+  Count_rows = dbh.select_all SQL  
 end
 
-puts Count_rows.inspect
+for row in Count_rows
+  puts "'#{row['Intersection']}' #{row['From'][0..0]}: "  + 
+       format("%f %f %f",row['L'],row['T'],row['R'])
+  sum = row['L']+row['T']+row['R']
+  raise "Warning: this rows turning probabilities summed to #{sum}!" if (sum-1.0).abs > 0.01
+end
