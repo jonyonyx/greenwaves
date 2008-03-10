27c7bbd294e8a36eeed8511818073b6589e8c67b
andreas.warberg <andreas.warberg@bbe71f41-d438-0410-94a7-597091fd6986>
2008-03-10 15:04:20 +0000

    separate rutebeslutninger for biler og lastbiler. dogs master genereres med kode og er mere nøjagtig jf systembeskrivelse (separat logik for indkobling adaptiv styring ift niveauvalg)

--- code/dogs_threshold.rb
+++ code/dogs_threshold.rb
@@ -3,44 +3,35 @@
  
 require 'const'
 
-puts "BEGIN"
+def get_thresholds(detectors)
 
-DOGS_LEVELS = 8
-DOGS_LEVELDOWN_BUFFER = 0.1 # percentage of threshold value for current level
-BASE_CYCLE_TIME = 80 # seconds
+  acc_xls = "#{Herlev_dir}aggr.xls"
+  aggr_CS = "DBI:ADO:Provider=Microsoft.Jet.OLEDB.4.0;Data Source=#{acc_xls};Extended Properties=\"Excel 8.0;HDR=Yes;IMEX=1\";"
 
-Acc_xls = "#{Herlev_dir}aggr.xls"
-Aggr_CS = "DBI:ADO:Provider=Microsoft.Jet.OLEDB.4.0;Data Source=#{Acc_xls};Extended Properties=\"Excel 8.0;HDR=Yes;IMEX=1\";"
-
-
-th = {}
-
-for det in [['D3'],['D14']]#[0..0]  
   # fetch historical traffic input data in 15m granularity
   rows = exec_query "SELECT AVG(Detected) As Q 
          FROM [data$] 
          WHERE DoW IN ('Mon','Tue','Wed','Thu','Fri') AND 
-         Detector IN ('#{det.join("', '")}') AND
+         Detector IN ('#{detectors.join("', '")}') AND
          Time BETWEEN \#1899/12/30 07:00:00\# AND \#1899/12/30 09:00:00\#
-         GROUP BY Time" , Aggr_CS
+         GROUP BY Time" , aggr_CS
   
   q = rows.map{|r| r['Q']}
   
-  puts "#{det.join(',')}: [#{q.min},#{q.max}]"
-  
-  ubound_ar = []
-  lbound_ar = []
-  th[det] = {'UBOUND' => ubound_ar, 'LBOUND' => lbound_ar}
+ # puts "#{detectors.join(',')}: [#{q.min},#{q.max}]"
+  ubounds,lbounds = [],[]
   q.min.step(q.max, (q.max - q.min) / (DOGS_LEVELS.to_f - 1)) do |bound| # sum over 15min
     # scale down to match a BASE_CYCLE_TIME period
     ub = (bound * BASE_CYCLE_TIME / (Res*60.0)).round
-    ubound_ar << ub
+    ubounds << ub
     # set lower bound 10% below upper bound to avoid hystereses ie. rapid changing 
     # between adjacent dogs levels!
-    lbound_ar << (ub - 0.1*ub).round 
+    lbounds << (ub - DOGS_LEVELDOWN_BUFFER*ub).round 
   end
+  [ubounds,lbounds]
 end
 
-print ['UBOUND','LBOUND'].map{|btype| "\t#{btype}_CNT_CRIT [2,#{DOGS_LEVELS}] = " + th.values.map{|h| h[btype]}.inspect}.join(",\n"),";"
-
+if __FILE__ == $0
+  puts get_thresholds(['D3']).inspect
+end
 
--- code/main.rb
+++ code/main.rb
@@ -2,6 +2,6 @@ require 'vissim'
 require 'turningprob'
 require 'vissim_input'
 
-get_inputs.write
+#get_inputs.write
 get_vissim_routes.write
-require 'vap'
+#require 'vap'
