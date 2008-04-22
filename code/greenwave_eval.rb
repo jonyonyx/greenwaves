b4bf51236c3a33c2969ee799cb82b5b45789c76b
andreas.warberg <andreas.warberg@bbe71f41-d438-0410-94a7-597091fd6986>
2008-04-22 15:50:05 +0000

    simulated annealing for finding offsets. gui to display wavebands.

--- code/greenwave_eval.rb
+++ code/greenwave_eval.rb
@@ -1,41 +1,40 @@
 require 'const'
-require 'wx'
-include Wx
 
 ARTERIAL = 'a'
 
 class SC
-  def state t
+  attr_accessor :offset
+  def initialize
+    @offset = 0    
+  end
+  def state t, o
     # convert the 1-based horizon second to 0-based plan indexing
     # offsets denote the delay in which the program should be started
     # and should thus be subtracted
-    t_loc = (t - @offset) % @plan.length 
+    t_loc = (t - o) % @plan.length 
     @plan[t_loc]
   end
-  def bands_in_horizon h
+  def bands_in_horizon h, o
     t = h.min
     
     while t <= h.max
-      if state(t) == ARTERIAL
+      if state(t, o) == ARTERIAL
         tstart = t
-        t += 1 while state(t) == ARTERIAL
+        t += 1 while state(t, o) == ARTERIAL
         tend = t      
         yield tstart, tend        
       end
       t += 1
-    end
-    
+    end    
   end
   def to_s
-    "SC#{@number.to_s}"
+    "Signal Controller #{@number}"
+  end
+  def <=> sc2
+    @number <=> sc2.number
   end
 end
 class Coordination
-  class Mismatch
-  end
-  def initialize
-    @mismatches = []
-  end
   # distance from stop-line to stop-line
   def distance
     dist = (@sc1.position - @sc2.position).abs
@@ -52,17 +51,20 @@ class Coordination
   def left_to_right
     @sc1.position < @sc2.position
   end
-  def mismatches_in_horizon h
+  # returns times where there is a mismatch between sc1 and sc2 ie.
+  # when traffic emitted from sc1 is not received by green light at sc2
+  # respecting the current offsets of these controllers and the traveltime
+  # from stop-light to stop-light
+  def mismatches_in_horizon h, o1, o2
     tt = traveltime
     
-    conflicts = (h.min..h.max).to_a.find_all{|t| @sc1.state(t) == ARTERIAL and not @sc2.state(t+tt) == ARTERIAL}
+    conflicts = (h.min..h.max).to_a.find_all{|t| @sc1.state(t,o1) == ARTERIAL and not @sc2.state(t+tt,o2) == ARTERIAL}
         
-    puts conflicts.inspect
     i = 0
     while i < conflicts.size - 1
       tstart = conflicts[i]
       i += 1 while conflicts[i] + 1 == conflicts[i+1]
-      yield tstart, conflicts[i]
+      yield tstart + tt, conflicts[i] + tt
       i += 1
     end
   end
@@ -74,192 +76,190 @@ class Coordination
       @sc2.position + @sc2.internal_distance
     end
   end
-  def check_mismatch t    
-    
-    tt = traveltime.round    
-      
-    #puts "#{t}: #{from.state(t,@offset)} vs. #{t+tt}: #{sc2.state(t+tt,@offset)}"
-    #puts "At second #{t}: SC1 in #{sc1.state(t,@offset)}, SC2 in #{sc2.state(t,@offset)}"
-    unless @sc1.state(t) == GREEN and not @sc2.state(t+tt) == GREEN      
-      dist = left_to_right ? @sc2.position : @sc1.position + @sc1.internal_distance
-      @mismatches << Mismatch.new!(:from => @sc1, :to => @sc2, :conflict_time => t+tt, :at_distance => dist)
-    end
-  end
-  def print_mismatches
-    for mm in @mismatches.sort{|mm1,mm2| mm1.from_time <=> mm2.from_time} # sort by second
-      from, to = mm.from, mm.to
-      puts "Traffic from #{from} in second #{mm.from_time} is not received by #{to} in second #{mm.from_time + to.offset}"
-    end
-  end
-  def eval
-    @mismatches.size
-  end
-  def report    
-    mismatchcount = @mismatches.find_all{|mm|mm.from == @sc1 and mm.to == @sc2}.length
-    puts "Found #{mismatchcount} mismatches from #{@sc1} to #{@sc2}" if mismatchcount > 0 
+  # finds the utility ie. number of mismatches in the given horizon
+  def eval h,o1,o2
+    z = 0
+    mismatches_in_horizon(h,o1,o2){|t1, t2| z += t2 - t1}
+    z
   end
   def to_s
     "Coordination between #{@sc1} and #{@sc2}"
   end
 end
 
-H = [0,50] # horizon
+H = [0,75] # horizon
 
 CONTROLLERS = [
-  {:number => 1, :plan => [ARTERIAL] * 10 + [RED] * 10, :position => 0, :internal_distance => 5,   :offset => 0},
-  {:number => 2, :plan => [ARTERIAL] * 10 + [RED] * 10, :position => 20, :internal_distance => 5,  :offset => 0},
-  {:number => 3, :plan => [ARTERIAL] * 10 + [RED] * 10, :position => 40, :internal_distance => 5,  :offset => 10}
+  {:number => 1, :plan => [ARTERIAL] * 10 + [RED] * 10, :position => 0, :internal_distance => 5},
+  {:number => 2, :plan => [ARTERIAL] * 10 + [RED] * 10, :position => 100, :internal_distance => 5},
+  {:number => 3, :plan => [ARTERIAL] * 10 + [RED] * 10, :position => 200, :internal_distance => 5}
 ]
-
-scs = CONTROLLERS.map{|opts| SC.new! opts}
 
 RELATIONS = [
-  {:scno1 => 1, :scno2 => 2, :velocity => 4.0, :color => BLUE},
-  {:scno1 => 2, :scno2 => 1, :velocity => 4.0, :color => BLACK},
-  {:scno1 => 2, :scno2 => 3, :velocity => 4.0, :color => CYAN},
-  {:scno1 => 3, :scno2 => 2, :velocity => 4.0, :color => BLACK}
+  {:scno1 => 1, :scno2 => 2, :velocity => 4.0},
+  {:scno1 => 2, :scno2 => 1, :velocity => 6.0},
+  {:scno1 => 2, :scno2 => 3, :velocity => 3.0},
+  {:scno1 => 3, :scno2 => 2, :velocity => 4.0}
 ]
 
-coordinations = RELATIONS.map do |opts| 
-  sc1 = scs.find{|sc| sc.number == opts[:scno1]}
-  sc2 = scs.find{|sc| sc.number == opts[:scno2]}
-  Coordination.new! opts.merge(:sc1 => sc1, :sc2 => sc2)
+def parse_coordinations
+  scs = CONTROLLERS.map{|opts| SC.new! opts}
+  
+  coordinations = RELATIONS.map do |opts| 
+    sc1 = scs.find{|sc| sc.number == opts[:scno1]}
+    sc2 = scs.find{|sc| sc.number == opts[:scno2]}
+    Coordination.new! opts.merge(:sc1 => sc1, :sc2 => sc2)
+  end
+  
+  yield coordinations, scs
 end
 
-#puts "Reporting relation performance in time horizon #{H.min} to #{H.max}"
-#for rel in coordinations
-#  #rel.print_mismatches
-#  puts rel.eval
-#  #rel.report
-#end
-#exit()
-
-class RoadTimeDiagram < Frame
-  FATPENWIDTH = 3
-  def initialize scs, coords, h
-    @signal_controllers = scs
-    @coordinations = coords
-    @horizon = h
-    
-    @tmax = @coordinations.map{|c| c.traveltime  + [c.sc1.plan.length,c.sc2.plan.length].max}.sum.to_f
-    @distmax = @signal_controllers.map{|sc| sc.position + sc.internal_distance}.max.to_f + 5
-          
-    @ox, @oy = 25, 25 # origo (0,0)
-      
-    @fat_pen = Pen.new
-    @fat_pen.set_width FATPENWIDTH
-    
-    @normal_pen = Pen.new
-    
-    @pens = {} # make a pen for each coordination
-    coords.each{|c| pen = Pen.new(c.color); pen.set_width(FATPENWIDTH); @pens[c] = pen}
-    
-    @green_pen = Pen.new(Colour.new(0,255,0))
-    @green_pen.set_width FATPENWIDTH + 1
-    
-    @red_pen = Pen.new(Colour.new(255,0,0))
-    @red_pen.set_width FATPENWIDTH
-    
-    super(nil, :title => "Road-Time Diagram (#{CONTROLLERS.size} intersections)", :size => [500,400])
-    evt_paint { on_paint }
-    evt_size { on_paint }
+class SimulatedAnnealing
+  def initialize problem, time_limit, params
+    @problem = problem
+    @time_limit = time_limit
+    @parameters = params
   end
-  def on_paint
-    paint do | dc |
-      dc.clear
+  def run
+    start_time = Time.now
+    
+    iterations = 0
+    
+    @problem.create_initial_solution
+    
+    temp = @parameters[:start_temp]
+    
+    while (Time.now - start_time) < @time_limit
+      iterations += 1
+      currentval = @problem.evaluate
       
-      siz = dc.size
-      h, w = siz.height, siz.width
-            
-      dc.set_pen @fat_pen
+      # change "current" directly to "neighbor" (save time copying objects)
+      # (@problem is capable of undoing the change)
+      @problem.change
       
-      dc.draw_line(0,h - @oy,w, h - @oy) # draw x-axis
-      dc.draw_line(@ox,h,@ox,0) # draw y-axis
-      
-      dc.set_pen @normal_pen
-      
-      # calculate the time frame (y-axis)
-      yscale = (dc.size.height - @oy)/ @tmax
-      
-      # distance from first to last intersection (x-axis)
-      xscale = (w - @ox) / @distmax
-      
-      # draw vertical lines (distance)
-      5.step(@distmax.round,5) do |d|
-        x = (d * xscale).round + @ox
-        dc.draw_text("#{d}", x + 1, h - @oy + 1)
-        dc.draw_line(x,h,x,0)
+      neighborval = @problem.evaluate
+
+      if neighborval < currentval 
+        # use the neighbor solution next iteration if it is better...
+        yield neighborval, currentval if block_given?
+        @problem.store_encumbent
+      elsif Math.exp(-(neighborval - currentval) / temp) > rand 
+        # and maybe even if its not!
+        # (retain neighbor solution as current)
+      else
+        # undo the change we made / return to previous "current" solution
+        @problem.undo_changes
       end
+
+      temp *= @parameters[:alpha]
+    end
+    
+    {:solution => @problem.prepare_solution, :iterations => iterations, :time => Time.now - start_time}
+  end
+end
+
+class CoordinationProblem
+  attr_reader :encumbent
+  def initialize coords, scs, horizon
+    @coordinations = coords
+    @signal_controllers = scs
+    @horizon = horizon
+    @size = @signal_controllers.size
+  end
+  # return a set of offsets for each signal controllers
+  def create_initial_solution
+    @current = {}
+    @signal_controllers.each{|sc| @current[sc] = 0}
+    store_encumbent
+    @coord_contribution = {}
+    @value = full_evaluation # updates current solution value
+  end
+  # make a clean copy of the current solution free from backreferences
+  def store_encumbent    
+    @encumbent = deepcopy(@current)
+  end
+  # make a change in the current solution
+  # note the change so that it may be undone, if requested
+  def change
+    # pick a coordination to alter
+    @changed_sc = @signal_controllers[(rand * @size).round % @size]
+    @previous_setting = @current[@changed_sc]
+    @current[@changed_sc] = @previous_setting + ((@previous_setting == 0 or rand < 0.5) ? 1 : -1)
+        
+    #@value = full_evaluation
+    
+    # perform delta-evaluation
+    
+    @delta_contribution = {}
+    # find the coordinations which involve one of these signal controllers
+    for coord in @coordinations.find_all{|c| [c.sc1, c.sc2].include? @changed_sc}
+      prev_contrib = @coord_contribution[coord]
+      @delta_contribution[coord] = prev_contrib
+      @value -= prev_contrib
       
-      # draw horizontal lines (time)
-      5.step(@tmax.round,5) do |t|
-        y = h - (t * yscale).round - @oy
-        dc.draw_text("#{t}", 1, y + 2)
-        dc.draw_line(0,y,w,y)
-      end
+      o1 = @current[coord.sc1]
+      o2 = @current[coord.sc2]
+      newval = coord.eval(@horizon, o1, o2)
       
-      @coordinations.each do |coord|
-        # draw a band where the width corresponds to the green time
-        # in the arterial direction        
-        
-        # stop the wave band this many pixels before the next intersection
-        proximity_spacer = 10 
-        sc1, sc2 = coord.sc1, coord.sc1
-        if coord.left_to_right
-          lstart = sc1.position
-          lend = lstart + coord.distance
-          proximity_spacer *= -1
-        else
-          lstart = sc2.position + sc2.internal_distance
-          lend = lstart - coord.distance
-        end
-        sc1.bands_in_horizon(@horizon) do |t1, t2|
-          #puts "t1 = #{t1}, t2 = #{t2}"
-          t1, t2 = t2, t1 unless coord.left_to_right
-                  
-          dc.set_pen @pens[coord]
-        
-          x1 = (lstart * xscale).round + @ox
-          x2 = (lend * xscale).round + @ox + proximity_spacer
-          y1 = h - @oy - (t1 * yscale).round
-          y2 = h - @oy - ((t1 + coord.traveltime) * yscale).round
-          ydelta = ((t2 - t1)*yscale).round # width of the band
-          
-          # the slope is given by the travel time
-          #ydelta = (coord.traveltime*yscale).round
-          
-          #          unless coord.left_to_right
-          #            y1, y2 = y2, y1
-          #          end
-          
-          dc.draw_line(x1, y1, x2, y2)
-          dc.draw_line(x1, y1 - ydelta, x2, y2 - ydelta)
-          
-          # draw a line where the wavefront meets the next signal controller
-          #dc.draw_line(x2, y1 - ydelta, x2, y2 - ydelta)
-          
-          # draw a line for the green signal duration
-          dc.set_pen @green_pen
-          dc.draw_line(x1, y1, x1, y1 - ydelta)          
-          
-        end
-        
-        next unless coord.left_to_right
-        dc.set_pen @red_pen
-        
-        x = (coord.conflict_position * xscale).round + @ox
-        # paint all mismatches (wave bands meeting a red light)
-        coord.mismatches_in_horizon(@horizon) do |t1, t2|
-          puts "Conflict in #{coord} from #{t1} to #{t2}"
-          dc.draw_line(x - 3, h - @oy - (t2 * yscale).round, x - 3, h - @oy - (t1 * yscale).round)
-        end
-      end
-      
+      @coord_contribution[coord] = newval
+      @value += newval
+    end
+    
+#    true_value = full_evaluation
+#    unless true_value == @value
+#      raise "Delta evaluation gives #{@value}, should be #{true_value}" 
+#    end
+  end
+  # return a hash of signals mapping to the found offsets
+  def prepare_solution
+    @current    
+  end
+  # undo all changes made during last call to change
+  def undo_changes
+    # restore state of previous solution
+    @current[@changed_sc] = @previous_setting
+    
+    #@value = full_evaluation
+    
+    # delta-restore the value of the solution
+    for coord, prev_contrib in @delta_contribution
+      @value -= @coord_contribution[coord]
+      @coord_contribution[coord] = prev_contrib
+      @value += prev_contrib
     end
   end
+  def evaluate
+    @value
+  end
+  # evaluate the current solution
+  def full_evaluation
+    @coordinations.each do |coord|
+      o1 = @current[coord.sc1]
+      o2 = @current[coord.sc2]
+      @coord_contribution[coord] = coord.eval(@horizon, o1, o2)
+    end
+    @coord_contribution.values.sum
+  end
 end
 
-App.run do
-  RoadTimeDiagram.new(scs, coordinations, H).show
+if __FILE__ == $0
+  parse_coordinations do |coords, scs|
+    problem = CoordinationProblem.new(coords, scs, H)
+    siman = SimulatedAnnealing.new(problem, 1, :start_temp => 100.0, :alpha => 0.5)
+        
+    #require 'profile'
+    result = siman.run do |newval, prevval|      
+      puts "Found new encumbent #{newval} vs. #{prevval}"
+    end
+    
+    puts "Solver completed in #{result[:time]} seconds after #{result[:iterations]} interations"
+    
+    puts "Found solution:"
+    for sc, offset in result[:solution].sort{|e1, e2| e1.first <=> e2.first}
+      puts "#{sc} offset: #{offset}"
+    end
+    
+    #require 'greenwave_gui'
+    #show_roadtime_diagram(coords, scs, H, result[:solution])
+  end
 end
-  
---
+++ code/greenwave_gui.rb
@@ -0,0 +1,157 @@
+
+require 'wx'
+include Wx
+
+class RoadTimeDiagram < Frame
+  FATPENWIDTH = 3
+  OPA = 192
+  def initialize coords, scs, h, offsets
+    super(nil, :title => "Road-Time Diagram (#{CONTROLLERS.size} intersections)", :size => [800,600])
+    
+    @signal_controllers = scs
+    @coordinations = coords
+    @horizon = h
+    @offset = offsets
+    
+    @tmax = [@coordinations.map{|c| c.traveltime}.sum, @horizon.max].max.to_f
+    @distmax = @signal_controllers.map{|sc| sc.position + sc.internal_distance}.max.to_f + 5
+          
+    # all time/distance related paint jobs must translat to match this origo (0,0)
+    @ox, @oy = 30, 120
+      
+    @fat_pen = Pen.new(BLACK)
+    @fat_pen.set_width FATPENWIDTH
+    
+    @normal_pen = Pen.new(BLACK)
+    
+    @greenwave_brush = Brush.new(Colour.new(0,255,255,160))
+    @bluewave_brush = Brush.new(Colour.new(0,0,255,160))
+    set_background_colour WHITE
+    
+    @green_pen = Pen.new(Colour.new(0,255,0))
+    @green_pen.set_width FATPENWIDTH * 2
+    
+    @red_pen = Pen.new(Colour.new(255,0,0))
+    @red_pen.set_width FATPENWIDTH * 2
+    
+    evt_paint { on_paint }
+    evt_size { on_paint }
+    show
+  end
+  def on_paint
+    paint do | dc |
+      dc.clear
+
+      siz = dc.size
+      h, w = siz.height, siz.width
+      
+      gdc = GraphicsContext.create(dc)  
+            
+      gdc.set_pen @fat_pen
+      
+      ybase = h - @oy # base offset for vertical drawing ops
+      
+      gdc.stroke_line(0,ybase,w, ybase) # draw x-axis
+      gdc.stroke_line(@ox,h,@ox,0) # draw y-axis
+      
+      dc.set_pen @normal_pen
+      
+      # scaling factor of time to pixels on the y-axis
+      yscale = ybase / @tmax
+      
+      # scaling factor of distance to pixels on the x-axis
+      xscale = (w - @ox) / @distmax
+      
+      # draw distance helper lines (vertical)
+      10.step(@distmax.round,10) do |d|
+        x = (d * xscale).round + @ox
+        dc.draw_text("#{d}", x + 1, ybase)
+        dc.draw_line(x,h,x,0)
+      end
+      
+      # draw time helper lines (horizontal)
+      10.step(@tmax.round,10) do |t|
+        y = ybase - (t * yscale).round
+        dc.draw_text("#{t}", 1, y)
+        dc.draw_line(0,y,w,y)
+      end
+      
+      # insert the names of the intersections
+      for sc in @signal_controllers
+        dc.draw_rotated_text(sc.to_s, (sc.position * xscale).round + @ox, h - 5, 90)
+      end    
+
+      green_lights = []
+      
+      # draw a band where the width corresponds to the green time
+      # in the arterial direction       
+      @coordinations.each do |coord|
+        
+        gdc.set_brush(coord.left_to_right ? @greenwave_brush : @bluewave_brush) # for filling wave bands
+      
+        sc1, sc2 = coord.sc1, coord.sc2
+        if coord.left_to_right
+          lstart = sc1.position
+          lend = lstart + coord.distance
+        else
+          lstart = sc1.position + sc1.internal_distance
+          lend = lstart - coord.distance
+        end
+        sc1.bands_in_horizon(@horizon, @offset[sc1]) do |t1, t2|
+          t1, t2 = t2, t1 unless coord.left_to_right
+        
+          x1 = (lstart * xscale).round + @ox
+          x2 = (lend * xscale).round + @ox
+          y1 = ybase - (t1 * yscale).round
+          y2 = ybase - ((t1 + coord.traveltime) * yscale).round
+          ydelta = ((t2 - t1)*yscale).round # width of the band in pixel
+          
+          # mark the path of the green band
+          path = gdc.create_path
+          
+          path.move_to_point(x1,y1)
+          path.add_line_to_point(x2,y2)
+          path.add_line_to_point(x2,y2 - ydelta)
+          path.add_line_to_point(x1,y1 - ydelta)
+          path.add_line_to_point(x1,y1)          
+          
+          gdc.set_pen(@fat_pen) # for drawing edges of the bands
+          
+          # outline the marked band using current pen then fill using green brush
+          gdc.fill_path(path)       
+          
+          green_lights << [x1, y1, x1, y1 - ydelta]
+        end
+        
+        # paint all mismatches (wave bands meeting a red light)
+        gdc.set_pen @red_pen
+        
+        x = (coord.conflict_position * xscale).round + @ox
+        coord.mismatches_in_horizon(@horizon, @offset[sc1], @offset[sc2]) do |t1, t2|
+          gdc.stroke_line(x, ybase - (t2 * yscale).round, x, ybase - (t1 * yscale).round)
+        end
+      end
+      
+      # draw a line for the green signal duration
+      gdc.set_pen @green_pen
+      for x1, y1, x2, y2 in green_lights
+        gdc.stroke_line(x1, y1, x2, y2)
+      end
+      
+    end
+  end
+end
+
+def show_roadtime_diagram coords, scs, horizon, offsets  
+  App.run do
+    RoadTimeDiagram.new(coords, scs, horizon, offsets)
+  end
+end
+
+if __FILE__ == $0
+  require 'greenwave_eval'
+  parse_coordinations do |coordinations, scs|
+    show_roadtime_diagram(coordinations, scs, H, Hash.new(0))
+  end
+end
+  
