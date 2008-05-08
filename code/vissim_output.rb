83cb52b7c4ef1bf3f0ff9d35b2d717391c2f5b86
andreas.warberg <andreas.warberg@bbe71f41-d438-0410-94a7-597091fd6986>
2008-05-08 16:29:57 +0000

    vissim_output

--- code/vissim_output.rb
+++ code/vissim_output.rb
@@ -1,15 +1,18 @@
+
 module VissimOutput 
   def write
-    section_contents = to_vissim # make sure this can be successfully generated
-    FileUtils.cp Default_network, "#{ENV['TEMP']}\\#{Network_name}#{rand}" # backup
-    inp = IO.readlines(Default_network)
+    section_contents = to_vissim # make sure this can be successfully generated before opening the network file!
+    network = Default_network
+    networkname = network.split("\\").last
+    FileUtils.cp network, "#{ENV['TEMP']}\\#{networkname}#{rand}" # backup
+    inp = IO.readlines(network)
     section_start = (0...inp.length).to_a.find{|i| inp[i] =~ section_header} + 1
     section_end = (section_start...inp.length).to_a.find{|i| inp[i] =~ /-- .+ --/ } - 1 
-    File.open(Default_network, "w") do |file| 
+    File.open(network, "w") do |file| 
       file << inp[0..section_start]
       file << "\n#{section_contents}\n"
       file << inp[section_end..-1]
     end
-    puts "Wrote #{self.class} to '#{Default_network}'"
+    puts "Wrote#{respond_to?(:length) ? " #{length}" : ''} #{self.class} to '#{network}'"
   end
 end
