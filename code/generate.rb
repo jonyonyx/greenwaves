dad49871ec767f718cab006d24617b061f70ccca
andreas.warberg <andreas.warberg@bbe71f41-d438-0410-94a7-597091fd6986>
2008-05-22 12:45:59 +0000

    corrected some mistakes at roskildevej. herlev hovedgade and gl landevej from east is now OK (HH even with normal signal plans ie. no after-green). gl landevej from west still troublesome.

---
+++ code/generate.rb
@@ -0,0 +1,29 @@
+require 'vissim'
+require 'turningprob'
+require 'vissim_input'
+require 'vap'
+require 'optparse'
+
+def usage
+  puts "Usage: #{$0.split(File::SEPARATOR).last} [all] | [vap | input | routes]"
+end
+
+def get_vissim_instance
+  @vissim ||= Vissim.new
+end
+
+#opts = OptionParser.new
+#opts.on('-h','--help') {usage}
+#opts.on('all') do 
+  vissim = get_vissim_instance
+  get_inputs(vissim).write
+  get_vissim_routes(vissim).write
+  generate_controllers(vissim)  
+#end
+#opts.on('vap'){generate_controllers(get_vissim_instance)}
+#opts.on('inputs'){get_inputs(get_vissim_instance).write}
+#opts.on('routes'){get_vissim_routes(get_vissim_instance).write}
+#
+#opts.parse(ARGV)
+
+
